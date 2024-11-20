4d3893e84aa6cda444f89f605238f82a2995cd14
Дарья <hzxz4610@gmail.com>
2024-11-20 18:02:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 filename = 'text.txt'
 
 with open(filename) as file:
-  text = file.read()
-  words = text.split()
-  countword = len(words)
-  print("В файле text.txt", countword, "слова.")
+    text = file.read()
+words = text.split()
+countword = len(words)
+print("В файле text.txt", countword, "слова.")
